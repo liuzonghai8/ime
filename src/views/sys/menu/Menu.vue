a7d05a31b0d274645502e8551d4e0da0cc0f5124
LZH147258 <369613662@qq.com>
2019-02-26 19:53:03 +0800

    menu

--- src/views/sys/menu/Menu.vue
+++ src/views/sys/menu/Menu.vue
@@ -1,33 +1,122 @@
 <template>
-  <v-layout>
-    <v-flex xs3>
-      <!-- :load-children="fetchUsers"  v-model="iteeee"  open-on-click :active.sync="active" activatable :open="open"  :active="active"-->
-      <v-treeview
-        :active.sync="active"
-        :items="items"
-        activatable
-        active-class="primary--text"
-        class="grey lighten-5"
-        transition
-        open-on-click
-      ></v-treeview>
-    </v-flex>
-    <v-divider vertical></v-divider>
-    <v-flex xs8>dddddddd</v-flex>
-  </v-layout>
+  <v-card>
+    <v-card-title class="py-1">
+      <v-btn small :color="dark ? 'secondary' : 'primary'" @click="handleadd">新增</v-btn>
+      <v-btn small :color="dark ? 'secondary' : 'primary'" @click="handleedit">编辑</v-btn>
+      <v-btn small :color="dark ? 'secondary' : 'primary'" @click="handledelete">删除</v-btn>
+    </v-card-title>
+    <v-layout justify-space-between>
+      <v-flex>
+        <!-- :load-children="fetchUsers"  v-model="iteeee"  open-on-click :active.sync="active" activatable :open="open"  :active="active"-->
+        <v-treeview
+          :active.sync="active"
+          :items="items"
+          activatable
+          active-class="primary--text"
+          class="grey lighten-5"
+          transition
+        ></v-treeview>
+      </v-flex>
+      <!-- <v-divider vertical></v-divider>
+      <v-flex xs8>dddddddd</v-flex>-->
+      <v-flex d-flex text-xs-center>
+        <v-scroll-y-transition mode="out-in">
+          <div
+            v-if="!menu"
+            class="title grey--text text--lighten-1 font-weight-light"
+            style="align-self: center;"
+          >请点击右边的树形菜单，选择一项</div>
+          <v-card v-else :key="menu.id" class="pt-4 mx-auto" max-width="700" flat>
+            <h3 class="blue--text headline mb-2">{{ menu.name }}</h3>
+            <v-divider></v-divider>
+            <!-- justify-space-between -->
+            <v-layout wrap justify-space-between>
+              <v-flex xs6 md5>
+                <v-text-field label="ID:" v-model.trim="menu.id" readonly></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="父ID:" v-model.trim="menu.parentId" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="标题:" v-model.trim="menu.name" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="权限标识:" v-model.trim="menu.permission" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="前端路径:" v-model.trim="menu.frontPath" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="页面组件:" v-model.trim="menu.component" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="请求URL:" v-model.trim="menu.requestUrl" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="请求方法:" v-model.trim="menu.requestMethod" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="排序:" v-model.trim="menu.sort" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-text-field label="类型:" v-model.trim="menu.type" :readonly="edit"></v-text-field>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-select :items="items" label="类型"></v-select>
+              </v-flex>
+              <v-flex xs6 md5>
+                <v-switch v-model="menu.type" :label="menu.type=='0' ? '0 ':'1'"></v-switch>
+              </v-flex>
+            </v-layout>
+            <v-layout class="my-2" row>
+              <v-btn @click="clear">重置</v-btn>
+              <v-spacer/>
+              <v-btn @click="submit" color="primary">保存</v-btn>
+            </v-layout>
+          </v-card>
+        </v-scroll-y-transition>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 <script>
 
 export default {
   name: 'sys-menu',
+  props: {
+    dark: Boolean
+  },
   data: () => ({
     active: [],
-    items: [] //数据集合
+    items: [], //数据集合，
+    menu: null,  //选择的数据
+    edit: false
+    //selected: false
   }),
   mounted () {
     this.getDataFromServer();
   },
+  watch: {
+    //监听数据的变化，数据有变化时刷新列表 // 监视pagination属性的变化
+    selected: {
+      deep: true,
+      handler () {
+        if (this.active.length) {
+          this.getMenuById()
+        }
+      }
+    }
+  },
+  computed: {
+    selected () {
+      if (!this.active.length) return undefined
+      const id = this.active[0]
+      this.getData().then(resp => { this.menu = resp })
+      return id
+    }
+  },
   methods: {
+    //获取全部数据
     getDataFromServer () {
       this.$axios.get("upms/sys/menu/tree").then((resp) => {
         console.log(resp.data)
@@ -41,18 +130,24 @@ export default {
           console.log(resp.data.msg)
         }
       })
-    }
-  },
-  watch: {
-    //监听数据的变化，数据有变化时刷新列表 // 监视pagination属性的变化
-    active: {
-      deep: true, // deep为true，会监视pagination的属性及属性中的对象属性变化
-      handler () {
-        // 变化后的回调函数，这里我们再次调用getDataFromServer即可
-        console.log("active")
-        console.log(this.active)
-      }
     },
+    //根据ID获取单个数据
+    getMenuById () {
+      this.$axios.get("upms/sys/menu/" + this.active[0])
+        .then((resp) => {
+          console.log(resp.data)
+          this.menu = resp.data.data
+        })
+    },
+    async getData () {
+      return await this.$axios.get("upms/sys/menu/" + this.active[0]).then((resp) => resp.data);
+    },
+
+    handleadd () { },
+    handleedit () { },
+    handledelete () { },
+    clear () { },
+    submit () { }
 
   }
 }
